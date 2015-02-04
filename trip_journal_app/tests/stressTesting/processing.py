91be70d1a66fb4ea31d15f4b29ba6356a730033d
Marichka <marichkadzhala@gmail.com>
2015-02-04 21:18:27 +0200

    Added refactored code to my branch

--- trip_journal_app/tests/stressTesting/processing.py
+++ trip_journal_app/tests/stressTesting/processing.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 handler = logging.FileHandler('multiprocessing.log')
 handler.setLevel(logging.INFO)
+
 # write logs to the file "multiprocessing.log"
 logger.addHandler(handler)
 
@@ -20,29 +21,9 @@ logger.info('Start execution of program')
 # Define an output queue
 output = mp.Queue()
 
-# define a example function
-#def rand_string(length, output):
-#    """ Generates a random string of numbers, lower- and uppercase chars. """
-#    rand_str = ''.join(random.choice(
-#                    string.ascii_lowercase
-#                    + string.ascii_uppercase
-#                    + string.digits)
-#               for i in range(length))
-#    output.put(rand_str)
-
-# Setup a list of processes that we want to run
+# opens a list of commands that we want to execute
 with open('script.sh', 'rb') as file:
     script = file.read()
 rc = subprocess.call(script, shell=True)
-# Run processes
-#for p in processes:
-#    p.start()
-
-# Exit the completed processes
-#for p in processes:
-#    p.join()
-
-# Get process results from the output queue
-#results = [output.get() for p in processes]
 
 logger.info('finish')
